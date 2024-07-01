2b5c2d934a7cbf99655d4023451c0bea8262a959
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-01 00:45:22 -0500

    adding more stuff

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
     return (
         <div className="flex justify-center my-20  ">
             <div className="">
-                <img src={imageHero} alt="Food-image" width={500} height={500} className="rounded-xl ml-10"/>
+                <img src={imageHero} alt="Food-image" width={500} height={500} className="box rounded-xl ml-10 "/>
             </div>
-            <div className="w-1/3">
+            <div className="w-1/3 font-bold">
                 <h1 className="text-4xl text-yellow-400 ml-20">A Feast for the Senses</h1>
-                <h1 className="text-4xl text-white ml-20">"Experience dining like never before with our exquisite menu and ambiance."</h1>
+                <h1 className="text-4xl text-gray-800 ml-20">"Experience dining like never before with our exquisite menu and ambiance."</h1>
             </div>
 
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import "./../App.css";
 
 const Navbar = () => {
     return (
-        <div className="flex justify-between bg-gray-200 mx-96 rounded-2xl mt-10 p-5 text-4xl font-bold">
+        <div className="flex justify-between bg-gray-500 mx-10 2xl:mx-96 rounded-3xl mt-10 p-5 text-4xl font-bold">
             <div className="">
                 <h1 className=" text-yellow-400 inline ">ServeDin</h1>
             </div>
-            <div className="">
-                <h1 className="inline mr-10">Home</h1>
-                <h1 className="inline mr-10">Menu</h1>
-                <h1 className="inline mr-10">Contact</h1>
+            <div className="text-white">
+                <a className="inline mr-10 cursor-pointer">Home</a>
+                <a className="inline mr-10 cursor-pointer">Menu</a>
+                <a className="inline mr-10 cursor-pointer">Contact</a>
             </div>
 
         </div>
