cef190c5a8aad11f773d8065ae9798c3cb348bac
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-03 00:36:22 -0500

    signature salads are done

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -4,6 +4,8 @@ import Navbar from './Navbar';
 import friedShrimp from "./fried-shrimp.jpg";
 import tots from "./tots.jpg";
 import crab from "./crab.jpg";
+import chicken from "./chicken.jpg";
+import salmon from "./salmon.jpg";
 
 const Food = () => {
     return (
@@ -27,6 +29,21 @@ const Food = () => {
                     <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Crab Cakes</p>
                     <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Hand pattied and grilled golden brown served with homemade remoulade sauce.</p>
                     <img src={crab} alt="crab" className="w-64 rounded-3xl m-auto"/>
+                    <hr className="mx-60 mt-5"/>
+                    <h2 className="text-center text-2xl text-white mt-5 mb-5 font-extrabold text-shadow">Appetizers</h2>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Cheese Curds</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Onion Rings</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chicken Strips</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Bavarian Pretzel</p>
+                    <hr className="mx-10 mt-10"/>
+                    <h2 className="text-center text-2xl text-white mt-10 mb-5 font-extrabold text-shadow">Signature Salads</h2>
+                    <hr className="mx-10 "/>
+                    <p className="text-center text-xl text-white mb-2 font-extrabold text-shadow">Chicken Cranberry Salad</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Grilled Chicken on a bed of mixed greens, with dried cranberries, scallions, Parmesan cheese, candied almonds & Balsamic vinaigrette .</p>
+                    <img src={chicken} alt="chicken" className="w-56 rounded-3xl m-auto "/>
+                    <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Blackened Salmon Salad</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Mixed greens topped with blackened salmon*, red onions, tomatoes, cucumbers, fresh mushrooms &  croutons.</p>
+                    <img src={salmon} alt="salmon" className="w-64 rounded-3xl m-auto"/>
                 </div>
             </div>
         </div>
