039977584fbc403dcc77a79c63c68fe9aa3ad224
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-08 22:17:36 -0500

    adding the burger section

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -7,6 +7,11 @@ import crab from "./crab.jpg";
 import chicken from "./chicken.jpg";
 import salmon from "./salmon.jpg";
 import Footer from './Footer';
+import drunkenDuck from "./../images/drunkenDuck.jpg";
+import hangoverDuck from "./../images/hangoverDuck.jpg";
+import classicDuck from "./../images/classicDuck.jpg";
+import buffalo from "./../images/buffalo.jpg";
+import giddyUp from "./../images/giddyUp.jpg";
 
 const Food = () => {
     return (
@@ -40,10 +45,46 @@ const Food = () => {
                     <h2 className="text-center text-2xl text-white mt-10 mb-5 font-extrabold text-shadow">Signature Salads</h2>
                     <p className="text-center text-xl text-white mb-2 font-extrabold text-shadow">Chicken Cranberry Salad</p>
                     <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Grilled Chicken on a bed of mixed greens, with dried cranberries, scallions, Parmesan cheese, candied almonds & Balsamic vinaigrette .</p>
-                    <img src={chicken} alt="chicken" className="w-56 rounded-3xl m-auto "/>
+                    <img src={chicken} alt="chicken" className="w-64 rounded-3xl m-auto "/>
                     <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Blackened Salmon Salad</p>
                     <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Mixed greens topped with blackened salmon*, red onions, tomatoes, cucumbers, fresh mushrooms &  croutons.</p>
                     <img src={salmon} alt="salmon" className="w-64 rounded-3xl m-auto"/>
+                    <hr className="mx-60 mt-5"/>
+                    <h2 className="text-center text-2xl text-white mt-5 mb-5 font-extrabold text-shadow">Salads</h2>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chicken Ceasar Salad</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Grilled chiken, romaine lettuce tossed with parmesan cheese, ceasar dressing &  croutons.</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Cobb Salad</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Iceberg lettuce topped with rows of grilled chicken, bacon, crumbled bleu cheese, red onions, cheddar, tomatoes & hard boiled eggs .</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chef Salad</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Iceberg lettuce, topped with deli ham, turkey, red onions, tomato, green peppers, cheese & egg.</p>
+                    <hr className="mx-10 mt-10"/>
+                    <h2 className="text-center text-2xl text-white mt-5 mb-2 font-extrabold text-shadow">Sandwich Favorites</h2>
+                    <p className="text-center text-base text-gray-200 mb-6 font-extrabold ">Served with Fries – Sub Cheese Curds or Waffle Fries for $2.50</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Clubhouse Sandwich</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Bacon, Lettuce & Tomato</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Grilled Chicken Sandwich</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Hand Breaded Walleye</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Irish Ruben</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Grilled Salmon Sandwich</p>
+                    <hr className="mx-10 mt-10"/>
+                    <h2 className="text-center text-2xl text-white mt-5 mb-2 font-extrabold text-shadow">100% Certified Angus Beef Burgers</h2>
+                    <p className="text-center text-base text-gray-200 mb-6 font-extrabold ">Served with Fries – Sub Cheese Curds or Waffle Fries for $2.50</p>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Drunken Duck Burger*</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">2 patties topped with bacon, ham, shredded chicken, swiss & cheddar cheese.</p>
+                    <img src={drunkenDuck} alt="drunkenDuck burger" className="w-64 rounded-3xl m-auto "/>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Hangover Duck Burger*</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Fried egg, hash browns, bacon & cheddar cheese.</p>
+                    <img src={hangoverDuck} alt="hangoverDuck burger" className="w-64 rounded-3xl m-auto "/>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">The Classic Duck Burger*</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Choice of cheese, topped with lettuce, tomato, onions & pickles.</p>
+                    <img src={classicDuck} alt="classicDuck burger" className="w-64 rounded-3xl m-auto "/>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Buffalo Burger*</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Pepper jack cheese, buffalo sauce, chipotle mayo, jalapenos & topped with crispy onions.</p>
+                    <img src={buffalo} alt="buffalo burger" className="w-64 rounded-3xl m-auto "/>
+                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Giddy Up Burger*</p>
+                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold ">Topped with BBQ sauce, cheddar cheese, strips of  bacon & a golden brown onion ring.</p>
+                    <img src={giddyUp} alt="giddyUp burger" className="w-64 rounded-3xl m-auto "/>
+                    <hr className="mx-10 mt-10"/>
                 </div>
             </div>
             <div>
