662793b0c897ed4fe9032b3470b78352c6a7d206
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-09 11:55:36 -0500

    adding the id's to the sections and linking them with the a tags

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -24,75 +24,86 @@ const Food = () => {
                 <hr className="mx-10" />
                 <div>
                     <div className="grid grid-cols-3 gap-2 text-center ">
-                        <a href="#sides"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter ">Signature Appetizers</button></a>
-                        <a href="#sides"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Appetizers</button></a>
-                        <a href="#sides"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Signature Salads</button></a>
-                        <a href="#sides"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Salads</button></a>
-                        <a href="#sides"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Sandwich Favorites</button></a>
-                        <a href="#sides"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Burgers</button></a>
+                        <a href="#signatureApps"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter ">Signature Appetizers</button></a>
+                        <a href="#apps"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Appetizers</button></a>
+                        <a href="#signatureSalads"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Signature Salads</button></a>
+                        <a href="#salads"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Salads</button></a>
+                        <a href="#sandwich"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Sandwich Favorites</button></a>
+                        <a href="#burgers"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Burgers</button></a>
                         <a href="#sides"><button className="mx-14 text-2xl text-white my-5 font-extrabold text-shadow border rounded-xl p-2 backdrop-filter">Side Choices</button></a>
                     </div>
-                    {/* Need to work on the id for the sections!!!!!! */}
                     <hr className="mx-10" />
-                    <h2 className="text-center text-3xl text-white mt-10 mb-5 font-extrabold text-shadow">Signature Appetizers</h2>
-                    <p className="text-center text-xl text-white mb-2 font-extrabold text-shadow">Bada Bing Shrimp</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">½ lb hand breaded, deep-fried shrimp served with signature sauce.</p>
-                    <img src={friedShrimp} alt="fried-shrimp" className="w-64 rounded-3xl m-auto" />
-                    <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Loaded Tater Tots</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">¾ lb golden-brown tater tots topped with our signature queso sauce, bacon, scallions & drizzled with sour cream.</p>
-                    <img src={tots} alt="tots" className="w-64 rounded-3xl m-auto" />
-                    <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Crab Cakes</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Hand pattied and grilled golden brown served with homemade remoulade sauce.</p>
-                    <img src={crab} alt="crab" className="w-64 rounded-3xl m-auto" />
+                    <div id="signatureApps">
+                        <h2 className="text-center text-3xl text-white mt-10 mb-5 font-extrabold text-shadow">Signature Appetizers</h2>
+                        <p className="text-center text-xl text-white mb-2 font-extrabold text-shadow">Bada Bing Shrimp</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">½ lb hand breaded, deep-fried shrimp served with signature sauce.</p>
+                        <img src={friedShrimp} alt="fried-shrimp" className="w-64 rounded-3xl m-auto" />
+                        <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Loaded Tater Tots</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">¾ lb golden-brown tater tots topped with our signature queso sauce, bacon, scallions & drizzled with sour cream.</p>
+                        <img src={tots} alt="tots" className="w-64 rounded-3xl m-auto" />
+                        <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Crab Cakes</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Hand pattied and grilled golden brown served with homemade remoulade sauce.</p>
+                        <img src={crab} alt="crab" className="w-64 rounded-3xl m-auto" />
+                    </div>
                     <hr className="mx-60 mt-5" />
-                    <h2 className="text-center text-3xl text-white mt-5 mb-5 font-extrabold text-shadow">Appetizers</h2>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Cheese Curds</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Onion Rings</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chicken Strips</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Bavarian Pretzel</p>
+                    <div id="apps">
+                        <h2 className="text-center text-3xl text-white mt-5 mb-5 font-extrabold text-shadow">Appetizers</h2>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Cheese Curds</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Onion Rings</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chicken Strips</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Bavarian Pretzel</p>
+                    </div>
                     <hr className="mx-10 mt-10" />
-                    <h2 className="text-center text-3xl text-white mt-10 mb-5 font-extrabold text-shadow">Signature Salads</h2>
-                    <p className="text-center text-xl text-white mb-2 font-extrabold text-shadow">Chicken Cranberry Salad</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Grilled Chicken on a bed of mixed greens, with dried cranberries, scallions, Parmesan cheese, candied almonds & Balsamic vinaigrette.</p>
-                    <img src={chicken} alt="chicken" className="w-64 rounded-3xl m-auto" />
-                    <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Blackened Salmon Salad</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Mixed greens topped with blackened salmon*, red onions, tomatoes, cucumbers, fresh mushrooms & croutons.</p>
-                    <img src={salmon} alt="salmon" className="w-64 rounded-3xl m-auto" />
+                    <div id="signatureSalads">
+                        <h2 className="text-center text-3xl text-white mt-10 mb-5 font-extrabold text-shadow">Signature Salads</h2>
+                        <p className="text-center text-xl text-white mb-2 font-extrabold text-shadow">Chicken Cranberry Salad</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Grilled Chicken on a bed of mixed greens, with dried cranberries, scallions, Parmesan cheese, candied almonds & Balsamic vinaigrette.</p>
+                        <img src={chicken} alt="chicken" className="w-64 rounded-3xl m-auto" />
+                        <p className="text-center text-xl text-white mt-10 mb-2 font-extrabold text-shadow">Blackened Salmon Salad</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Mixed greens topped with blackened salmon*, red onions, tomatoes, cucumbers, fresh mushrooms & croutons.</p>
+                        <img src={salmon} alt="salmon" className="w-64 rounded-3xl m-auto" />
+                    </div>
                     <hr className="mx-60 mt-5" />
-                    <h2 className="text-center text-3xl text-white mt-5 mb-5 font-extrabold text-shadow">Salads</h2>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chicken Ceasar Salad</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Grilled chicken, romaine lettuce tossed with parmesan cheese, ceasar dressing & croutons.</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Cobb Salad</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Iceberg lettuce topped with rows of grilled chicken, bacon, crumbled bleu cheese, red onions, cheddar, tomatoes & hard boiled eggs.</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chef Salad</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Iceberg lettuce, topped with deli ham, turkey, red onions, tomato, green peppers, cheese & egg.</p>
+                    <div id="salads">
+                        <h2 className="text-center text-3xl text-white mt-5 mb-5 font-extrabold text-shadow">Salads</h2>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chicken Ceasar Salad</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Grilled chicken, romaine lettuce tossed with parmesan cheese, ceasar dressing & croutons.</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Cobb Salad</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Iceberg lettuce topped with rows of grilled chicken, bacon, crumbled bleu cheese, red onions, cheddar, tomatoes & hard boiled eggs.</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Chef Salad</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Iceberg lettuce, topped with deli ham, turkey, red onions, tomato, green peppers, cheese & egg.</p>
+                    </div>
                     <hr className="mx-10 mt-10" />
-                    <h2 className="text-center text-3xl text-white mt-5 mb-2 font-extrabold text-shadow">Sandwich Favorites</h2>
-                    <p className="text-center text-base text-gray-200 mb-6 font-extrabold">Served with Fries – Sub Cheese Curds or Waffle Fries for $2.50</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Clubhouse Sandwich</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Bacon, Lettuce & Tomato</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Grilled Chicken Sandwich</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Hand Breaded Walleye</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Irish Ruben</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Grilled Salmon Sandwich</p>
+                    <div id="sandwich">
+                        <h2 className="text-center text-3xl text-white mt-5 mb-2 font-extrabold text-shadow">Sandwich Favorites</h2>
+                        <p className="text-center text-base text-gray-200 mb-6 font-extrabold">Served with Fries – Sub Cheese Curds or Waffle Fries for $2.50</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Clubhouse Sandwich</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Bacon, Lettuce & Tomato</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Grilled Chicken Sandwich</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Hand Breaded Walleye</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Irish Ruben</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Grilled Salmon Sandwich</p>
+                    </div>
                     <hr className="mx-10 mt-10" />
-                    <h2 className="text-center text-3xl text-white mt-5 mb-2 font-extrabold text-shadow">100% Certified Angus Beef Burgers</h2>
-                    <p className="text-center text-base text-gray-200 mb-6 font-extrabold">Served with Fries – Sub Cheese Curds or Waffle Fries for $2.50</p>
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Drunken Duck Burger*</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">2 patties topped with bacon, ham, shredded chicken, swiss & cheddar cheese.</p>
-                    <img src={drunkenDuck} alt="drunkenDuck burger" className="w-64 rounded-3xl m-auto" />
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Hangover Duck Burger*</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Fried egg, hash browns, bacon & cheddar cheese.</p>
-                    <img src={hangoverDuck} alt="hangoverDuck burger" className="w-64 rounded-3xl m-auto" />
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">The Classic Duck Burger*</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Choice of cheese, topped with lettuce, tomato, onions & pickles.</p>
-                    <img src={classicDuck} alt="classicDuck burger" className="w-64 rounded-3xl m-auto" />
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Buffalo Burger*</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Pepper jack cheese, buffalo sauce, chipotle mayo, jalapenos & topped with crispy onions.</p>
-                    <img src={buffalo} alt="buffalo burger" className="w-64 rounded-3xl m-auto" />
-                    <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Giddy Up Burger*</p>
-                    <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Topped with BBQ sauce, cheddar cheese, strips of bacon & a golden brown onion ring.</p>
-                    <img src={giddyUp} alt="giddyUp burger" className="w-64 rounded-3xl m-auto" />
+                    <div id="burgers">
+                        <h2 className="text-center text-3xl text-white mt-5 mb-2 font-extrabold text-shadow">100% Certified Angus Beef Burgers</h2>
+                        <p className="text-center text-base text-gray-200 mb-6 font-extrabold">Served with Fries – Sub Cheese Curds or Waffle Fries for $2.50</p>
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Drunken Duck Burger*</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">2 patties topped with bacon, ham, shredded chicken, swiss & cheddar cheese.</p>
+                        <img src={drunkenDuck} alt="drunkenDuck burger" className="w-64 rounded-3xl m-auto" />
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Hangover Duck Burger*</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Fried egg, hash browns, bacon & cheddar cheese.</p>
+                        <img src={hangoverDuck} alt="hangoverDuck burger" className="w-64 rounded-3xl m-auto" />
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">The Classic Duck Burger*</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Choice of cheese, topped with lettuce, tomato, onions & pickles.</p>
+                        <img src={classicDuck} alt="classicDuck burger" className="w-64 rounded-3xl m-auto" />
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Buffalo Burger*</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Pepper jack cheese, buffalo sauce, chipotle mayo, jalapenos & topped with crispy onions.</p>
+                        <img src={buffalo} alt="buffalo burger" className="w-64 rounded-3xl m-auto" />
+                        <p className="text-center text-xl text-white mt-5 mb-2 font-extrabold text-shadow">Giddy Up Burger*</p>
+                        <p className="text-center text-base text-gray-300 mb-5 font-extrabold">Topped with BBQ sauce, cheddar cheese, strips of bacon & a golden brown onion ring.</p>
+                        <img src={giddyUp} alt="giddyUp burger" className="w-64 rounded-3xl m-auto" />
+                    </div>
                     <hr className="mx-10 mt-10" />
                     <div id="sides">
                         <h2 className="text-center text-3xl text-white mt-5 mb-2 font-extrabold text-shadow">Side Choices</h2>
