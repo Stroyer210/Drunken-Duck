b4511d82cf15331586f741b4ca27bc6d8b11ae01
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-26 11:41:28 -0500

    making the footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,13 @@ import {
 
 const Footer = () => {
     return (
-        <div className="flex justify-between  mx-10 2xl:mx-96 rounded-3xl mt-10 p-5 text-lg font-bold">
-            <div className="max-w-[1200px] sm:h-[150px] p-12 sm:flex justify-between mx-auto items-center inline-block text-center">
+        <div className="flex justify-between md:mx-10 2xl:mx-96 rounded-3xl mt-10 md:p-5 text-lg font-bold">
+            <div className="max-w-[1200px] sm:h-[150px] md:p-12 sm:flex justify-between mx-auto items-center inline-block text-center">
                     <span className="text-gray-300 dark:text-gray-600 biorhyme">
                     The Drunken Duck
                     </span>
 
-                    <div className="text-4xl flex sm:flex-row justify-center md:gap-16 py-3 text-gray-300 dark:text-white mt-3 mx-28 md:mt-0">
+                    <div className="text-4xl flex sm:flex-row justify-center lg:gap-16 py-3 text-gray-300 dark:text-white mt-3 mx-28 md:mt-0">
                         <a href="https://www.instagram.com/sthiwar_17/" target="_blank" rel="noreferrer">
                             <AiOutlineInstagram className="contact-shadow dark:text-gray-100 dark:hover:text-gray-900" />
                         </a>
