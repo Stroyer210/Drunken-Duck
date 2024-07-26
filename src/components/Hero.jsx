419a302324b833cb911a9b28386e80779be21665
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-26 11:14:46 -0500

    making the main section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,13 +11,13 @@ import logo2 from './../images/logo2.png';
 
 const Hero = () => {
     return (
-        <div className=" my-20  ">
+        <div className=" my-20 overflow-auto">
             <div className="text-center mb-10">
-                <h1 className="text-7xl text-white font-extrabold ml-20 text-shadow biorhyme"><img src={logo2} alt="mylogo" className="w-40 inline mr-10 pb-10"/>The Drunken Duck</h1>
+                <h1 className="text-4xl lg:text-7xl text-white font-extrabold ml-20 text-shadow biorhyme"><img src={logo2} alt="mylogo" className="w-40 inline mr-10 pb-10"/>The Drunken Duck</h1>
                 <p className="text-white mx-auto w-3/6 mt-6 text-2xl text-shadow">Explore our menus, read reviews, and find our location. <span className="font-bold"> We can't wait to serve you!</span></p>
             </div>
-            <div className="flex justify-center text-shadow">
-                <div className="mx-10">
+            <div className="md:grid md:grid-cols-1 lg:flex lg:justify-center text-shadow ">
+                <div className="mx-auto lg:mx-10">
                     <Link to="/Food">
                     <div className="backdrop-filter p-4 w-72 py-32 border-solid border-4 border-gray-700 rounded-2xl cursor-pointer" >
                         <h1 className="text-center text-4xl text-white font-extrabold h-auto">Food Menu</h1>
@@ -26,7 +26,7 @@ const Hero = () => {
                     </Link>
                 </div>
 
-                <div className="mx-10">
+                <div className="mx-auto my-10 lg:my-0 lg:mx-10">
                     <Link to="/Drink">
                     <div className="backdrop-filter p-4 w-72 py-32 border-solid border-4 border-gray-700 rounded-2xl cursor-pointer" >
                     <h1 className="text-center text-4xl text-white font-extrabold h-auto">Drink Menu</h1>
@@ -35,7 +35,7 @@ const Hero = () => {
                     </Link>
                 </div>
 
-                <div className="mx-10">
+                <div className="mx-auto lg:mx-10">
                     <Link to="/Touch">
                     <div className="backdrop-filter p-4 w-72 py-32 border-solid border-4 border-gray-700 rounded-2xl cursor-pointer" >
                     <h1 className="text-center text-4xl text-white font-extrabold h-auto">Get in Touch</h1>
